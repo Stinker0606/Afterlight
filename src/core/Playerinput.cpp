2b4338d5a42ee8b49ad7a9c8de29e04c64bede04
Will <willibecker56@gmail,com>
2025-05-26 01:27:07 +0200

    good

--- src/core/Playerinput.cpp
+++ src/core/Playerinput.cpp
@@ -3,57 +3,101 @@
 //
 
 #include "PlayerInput.h"
-#include "../config.h.in"
-#include <cmath> // für mathematische Funktionen (wie sqrtf)
+#include <cmath> // für mmathematische Funktionen (wie sqrtf)
 
-namespace game::core
+Player::Player(Vector2 start_Position, float move_Speed)
 {
-    // Spieler Input
-    Player::Player(Vector2 start_Position, float move_Speed)
-            : position(start_Position), speed(move_Speed)
-    {
-        //Steuerungstasten aus globaler Config
+    position = start_Position;
+    speed = move_Speed;
 
-        key_Up = game::Config::key_Up;
-        key_Down = game::Config::key_Down;
-        key_Left = game::Config::key_Left;
-        key_Right = game::Config::key_Right;
-    }
-
-    // Prüft Tasteneingabe, normalisiert bewegung, setzt Spieler Richtung
-    Player::Direction Player::Update(DT::timemachine& deltaTimeMachine)
-    {
-        Vector2 movement = {0.0f, 0.0f};
-
-        // Tasteneingabe prüfen
-        if (IsKeyDown(key_Up))    movement.y -= 1.0f;
-        if (IsKeyDown(key_Down))  movement.y += 1.0f;
-        if (IsKeyDown(key_Left))  movement.x -= 1.0f;
-        if (IsKeyDown(key_Right)) movement.x += 1.0f;
-
-        // Bewegung normalisieren (diagonal = gleiche Geschwindigkeit)
-        if (movement.x != 0.0f || movement.y != 0.0f)
-        {
-            float length = sqrtf(movement.x * movement.x + movement.y * movement.y);
-            movement.x /= length;
-            movement.y /= length;
-
-            float dt = deltaTimeMachine.Get_Dt();
-            position.x += movement.x * speed * dt;
-            position.y += movement.y * speed * dt;
-        }
-
-        // Richtung setzen
-        if (movement.x < 0 && movement.y < 0) lastDirection = LeftUp;
-        else if (movement.x > 0 && movement.y < 0) lastDirection = RightUp;
-        else if (movement.x < 0 && movement.y > 0) lastDirection = LeftDown;
-        else if (movement.x > 0 && movement.y > 0) lastDirection = RightDown;
-        else if (movement.x < 0) lastDirection = Left;
-        else if (movement.x > 0) lastDirection = Right;
-        else if (movement.y < 0) lastDirection = Up;
-        else if (movement.y > 0) lastDirection = Down;
-
-        return lastDirection;
-    }
+    //Steuerungstasten in die Config !
+    key_Up = KEY_W;
+    key_Down = KEY_S;
+    key_Left = KEY_A;
+    key_Right = KEY_D;
 }
 
+//Methode zur Steuerung der Bewegung. Wird jeden Frame aufgerufen
+Player::Direction Player::Update()
+{
+    Vector2 movement_Direction = {0.0f, 0.0f};
+
+    bool up = IsKeyDown(key_Up);
+    bool down = IsKeyDown(key_Down);
+    bool left = IsKeyDown(key_Left);
+    bool right = IsKeyDown(key_Right);
+
+    if (up) movement_Direction.y -= 1.0f;
+    if (down) movement_Direction.y += 1.0f;
+    if (left) movement_Direction.x -= 1.0f;
+    if (right) movement_Direction.x += 1.0f;
+
+    // Bewegung normalisieren
+    if (movement_Direction.x != 0.0f || movement_Direction.y != 0.0f)
+    {
+        float length = sqrtf(movement_Direction.x * movement_Direction.x + movement_Direction.y * movement_Direction.y);
+        movement_Direction.x /= length;
+        movement_Direction.y /= length;
+
+        position.x += movement_Direction.x * speed * GetFrameTime();
+        position.y += movement_Direction.y * speed * GetFrameTime();
+    }
+
+    if (IsKeyPressed(key_J) && !attacking)
+    {
+        attacking = true;
+        attackTimer = attackDuration;
+    }
+
+    if (attacking)
+    {
+        attackTimer -= GetFrameTime();
+        if (attackTimer <= 0.0f)
+        {
+            attacking = false;
+            attackTimer = 0.0f;
+        }
+    }
+    // Richtung zurückgeben
+    if (movement_Direction.x < 0 && movement_Direction.y < 0) lastDirection = LeftUp;
+    else if (movement_Direction.x > 0 && movement_Direction.y < 0) lastDirection = RightUp;
+    else if (movement_Direction.x < 0 && movement_Direction.y > 0) lastDirection = LeftDown;
+    else if (movement_Direction.x > 0 && movement_Direction.y > 0) lastDirection = RightDown;
+    else if (movement_Direction.x < 0) lastDirection = Left;
+    else if (movement_Direction.x > 0) lastDirection = Right;
+    else if (movement_Direction.y < 0) lastDirection = Up;
+    else if (movement_Direction.y > 0) lastDirection = Down;
+
+    return lastDirection;
+}
+Rectangle Player::GetAttackHitbox() const
+{
+    if (!attacking) return { 0, 0, 0, 0 };
+
+    float size = 20.0f;
+    Vector2 hitboxPos = position;
+
+    switch (lastDirection)
+    {
+        case Up:        hitboxPos.y -= size; break;
+        case Down:      hitboxPos.y += size; break;
+        case Left:      hitboxPos.x -= size; break;
+        case Right:     hitboxPos.x += size; break;
+        case LeftUp:    hitboxPos.x -= size; hitboxPos.y -= size; break;
+        case RightUp:   hitboxPos.x += size; hitboxPos.y -= size; break;
+        case LeftDown:  hitboxPos.x -= size; hitboxPos.y += size; break;
+        case RightDown: hitboxPos.x += size; hitboxPos.y += size; break;
+    }
+
+    return Rectangle{ hitboxPos.x - size / 2, hitboxPos.y - size / 2, size, size };
+}
+void Player::Draw()
+{
+    Rectangle playerRect = {
+        position.x - width / 2,
+        position.y - height / 2,
+        width,
+        height
+    };
+    DrawRectangleRec(playerRect, BLUE);
+}
--- src/core/Playerinput.h
+++ src/core/Playerinput.h
@@ -5,57 +5,60 @@
 #ifndef RAYLIBSTARTER_PLAYERINPUT_H
 #define RAYLIBSTARTER_PLAYERINPUT_H
 
+
 #include "raylib.h"
-#include "DeltaTimeMachine.h"
 
-//Vieles hier ist Teamspezifisch
-namespace game::core
+
+class Player
 {
-    class Player
+protected:
+    float width = 20.0f;
+    float height = 30.0f;
+public:
+
+    enum Direction
     {
-    public:
-        enum Direction
-        {
-            Up, Down, Left, Right, LeftDown, RightDown, LeftUp, RightUp
-        };
-
-        // Konstruktor initialisiert Spieler mit Start Position und Bewegungsgeschwindigkeit
-        Player(Vector2 start_Position, float move_Speed);
-
-        // Wird mit jedem Frame aufgerufen, um Position zu aktualisieren
-        Direction Update(DT::timemachine& deltaTimeMachine);
-
-        // Zeichnet den Spieler
-        void Draw();
-
-        // Gibt die aktuelle Spielerposition zurück
-        Vector2 GetPosition() const;
-
-    private:
-
-        // Aktuelle Position des Spielers
-        Vector2 position;
-
-        // Bewegungsgeschwindigkeit
-        float speed;
-
-        // Tastenzuweisung für die Bewegung
-        int key_Up;
-        int key_Down;
-        int key_Left;
-        int key_Right;
-
-        // Taste für den Nahkampfangriff
-        int key_Melee_Attack;
-
-        // Angriffssteuerung
-        bool attacking=false;
-        float attackDuration=0.3f;
-        float attackTimer = 0.0f;
-
-        // Zuletzt registrierte Bewegungsrichtung
-        Direction lastDirection = Down;
+        Up, Down, Left, Right, LeftDown, RightDown, LeftUp, RightUp
     };
-}
+
+    // Konstroktor initialisiert Spieler mit Start Position und Bewegungsgeschwindigkeit
+    Player(Vector2 start_Position, float move_Speed);
+
+    // Wird mit jedem Frame aufgerufen, um Position zu aktualisieren
+    Direction Update();
+
+    void DrawRectangle(Vector2 vector2, int pos_y, Color color);
+
+    //Zeichnet den Spieler
+    void Draw();
+    Vector2 GetPosition() const { return position; }
+
+    bool IsAttacking() const { return attacking; }
+    Rectangle GetAttackHitbox() const;
+
+private:
+
+    //Aktuelle Position des Spielers
+    Vector2 position;
+
+    //Bewegungsgeschwindigkeit
+    float speed;
+
+    //Tastenzuweoßung für die Bewegung
+    int key_Up;
+    int key_Down;
+    int key_Left;
+    int key_Right;
+
+    //Taste für den Angriff
+    int key_J;
+
+    bool attacking=false;
+    float attackDuration=0.3f;
+    float attackTimer = 0.0f;
+
+    Direction lastDirection = Down;
+};
+
 
 #endif //RAYLIBSTARTER_PLAYERINPUT_H
