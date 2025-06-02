eb00c4ca268ad1738a28c89123e6aba614f4f3f9
Will <willibecker56@gmail,com>
2025-06-02 22:32:18 +0200

    Aktualisierung

--- src/core/Playerinput.cpp
+++ src/core/Playerinput.cpp
@@ -6,7 +6,6 @@
 #include "../config.h.in"
 #include <cmath> // für mathematische Funktionen (wie sqrtf)
 
-//Vieles hier ist Teamspezifisch
 namespace game::core
 {
     // Spieler Input
@@ -19,16 +18,12 @@ namespace game::core
         key_Down = game::Config::key_Down;
         key_Left = game::Config::key_Left;
         key_Right = game::Config::key_Right;
-
-        key_Melee_Attack = game::Config::key_Melee_Attack;
     }
 
-    // Prüft Tasteneingabe, normalisiert bewegung, kontrolliert Angriffsdauer, setzt Spieler Richtung
+    // Prüft Tasteneingabe, normalisiert bewegung, setzt Spieler Richtung
     Player::Direction Player::Update(DT::timemachine& deltaTimeMachine)
     {
         Vector2 movement = {0.0f, 0.0f};
-
-        key_Melee_Attack = game::Config::key_Melee_Attack;
 
         // Tasteneingabe prüfen
         if (IsKeyDown(key_Up))    movement.y -= 1.0f;
@@ -48,24 +43,6 @@ namespace game::core
             position.y += movement.y * speed * dt;
         }
 
-        // Nahkampfangriff starten
-        if (IsKeyPressed(key_Melee_Attack) && !attacking)
-        {
-            attacking = true;
-            attackTimer = attackDuration;
-        }
-
-        // Angriff aktualisieren
-        if (attacking)
-        {
-            attackTimer -= deltaTimeMachine.Get_Dt();
-            if (attackTimer <= 0.0f)
-            {
-                attacking = false;
-                attackTimer = 0.0f;
-            }
-        }
-
         // Richtung setzen
         if (movement.x < 0 && movement.y < 0) lastDirection = LeftUp;
         else if (movement.x > 0 && movement.y < 0) lastDirection = RightUp;
@@ -78,62 +55,5 @@ namespace game::core
 
         return lastDirection;
     }
-
-    // Bestimmt die Nahkampfhittbox je nach Richtung
-    Rectangle Player::GetAttackHitbox() const
-    {
-        if (!attacking) return { 0, 0, 0, 0 };
-
-        const float size = 20.0f;
-        Vector2 hitboxPos = position;
-
-        switch (lastDirection)
-        {
-            case Up:        hitboxPos.y -= size; break;
-            case Down:      hitboxPos.y += size; break;
-            case Left:      hitboxPos.x -= size; break;
-            case Right:     hitboxPos.x += size; break;
-            case LeftUp:    hitboxPos.x -= size; hitboxPos.y -= size; break;
-            case RightUp:   hitboxPos.x += size; hitboxPos.y -= size; break;
-            case LeftDown:  hitboxPos.x -= size; hitboxPos.y += size; break;
-            case RightDown: hitboxPos.x += size; hitboxPos.y += size; break;
-        }
-
-        return Rectangle{ hitboxPos.x - size / 2, hitboxPos.y - size / 2, size, size };
-    }
-
-    // Bestimmt Erscheinungsbild des Spielers
-    void Player::Draw()
-    {
-        Rectangle playerRect =
-                {
-                        position.x - width / 2,
-                        position.y - height / 2,
-                        width,
-                        height
-                };
-        DrawRectangleRec(playerRect, BLUE);
-    }
 }
 
-    // Wenn der Spieler sich bewegt
-    if (movement_Direction.x != 0.0f || movement_Direction.y != 0.0f)
-    {
-        // Die Bewegung wird normalisiert, damit diagonale Bewegung nicht schneller ist
-        // Richting wird beibehalten, Stärke (Länge) des Vectors auf 1 gesetzt
-        float vectorLength = sqrtf((movement_Direction.x * movement_Direction.x) +
-                                   (movement_Direction.y * movement_Direction.y));
-
-        movement_Direction.x = movement_Direction.x / vectorLength;
-        movement_Direction.y = movement_Direction.y / vectorLength;
-    }
-
-    // Die Bewegung wird mit der Zeit pro Frame verrechnet,
-    // damit Geschwindigkeit unabhängig von der Framerate bleibt
-    float deltaTime = GetFrameTime();
-
-    //Neue Position berechnen
-    position.x += movement_Direction.x * speed * deltaTime;
-    position.y += movement_Direction.y * speed * deltaTime;
-
-}
