2fb801281d688ad7737d887fb832181d01ba519e
Andreas <manzandreas29@hotmail.com>
2025-07-07 09:43:12 +0200

    test

--- src/scenes/Screen.h
+++ src/scenes/Screen.h
@@ -21,7 +21,7 @@ public:
 private:
     std::unique_ptr<tson::Map> map;
     Texture2D tileatlas_Texture;
-    std::string levelmap_Path ="assets/graphics/data/Testmap1.json";
+    std::string levelmap_Path ="assets/maps/Swamp_1.json";
 };
 
 
