bd04cbe2118cc90124ad4eaea40de7af9eeb50bf
Andreas <manzandreas29@hotmail.com>
2025-08-11 04:56:31 +0200

    Working state 0.3.5.9
    Current State update:
    - Sokoban Walls fix, still some little bugs.
    
    Working:
    - Item Spawning
    - movWalls
    - Bombs
    - Heal + Dmg
    - Hit-Feedback
    - Spawner
    - Fog
    - Enemies
    - Animations
    - Dying
    
    Coming Next:
    - some bug fixes
    - melee or keys
    
    Not Working rn:
    - Projectile-collision Bomb is working with same collision??
    
    - Hp Drain? not tested
    
    - Hit Feedack doesnt work anymore.
    
    - Push Wall doesnt check small hitboxes.

--- src/game/interactables/PushBlock.cpp
+++ src/game/interactables/PushBlock.cpp
@@ -21,14 +21,18 @@ void Push_Block::Draw()
 
 void Push_Block::On_Collision(std::shared_ptr<Collidable> other)
 {
-    // Ignoriere Kollisionen mit dem Spieler
+    // Kollisionen mit dem Spieler werden von der Player-Klasse gehandhabt,
+    // hier also ignorieren, um Zyklen zu vermeiden.
     if (other->Get_Collision_Type() == Collision_Type::PLAYER) return;
 
-    // Wenn der Block mit einer Wand oder einem ANDEREN Block kollidiert
-    auto other_type = other->Get_Collision_Type();
-    if (other_type == Collision_Type::WALL || other_type == Collision_Type::ENEMY_SPAWNER)
+    // Prüfe, ob das andere Objekt "solide" ist.
+    // Ein Push_Block gibt selbst den Typ WALL zurück, also wird der erste Check
+    // auch die Kollision mit anderen Push_Blocks abfangen.
+    if (other->Get_Collision_Type() == Collision_Type::WALL ||
+        other->Get_Collision_Type() == Collision_Type::ENEMY_SPAWNER ||
+        other->Get_Collision_Type() == Collision_Type::ENEMY)
     {
-        // Setze die Position auf die Position VOR dem Push zurück
+        // Setze die Position auf die Position VOR dem Push zurück.
         this->hitbox.x = this->previous_position.x;
         this->hitbox.y = this->previous_position.y;
     }
