ee60a10225976fccb34b7365b73affba1e5bec0a
Andreas <manzandreas29@hotmail.com>
2025-09-10 14:14:10 +0200

    Working State 0.9.0 - for Game release
    Current State update:
    - Big Update - Dialog
    
    Working:
    - Item Spawning
    - movWalls
    - Bombs
    - Heal + Dmg for Player
    - Dmg for Enemies
    - Spawner
    - Fog
    - useFog mechanic
    - Enemies spawning
    - Animation Loading for Player
    - Dying State for Player
    - Projectiles for Player
    - Melee/Sweep for BOTH
    - UI
    - keyWall
    - HP Drain
    - breakWalls
    - Doors (Room Changing)
    - Score system
    - Animation Loading Enemy
    - Debug Placeholders for Enemies
    - Range Enemy
    - special enemy spawner
    - Spawn lags are away
    - Maps
    - reticle
    - Sound
    
    Coming Next:
    - Dialog system
    
    Not Working rn:
    - if kDebugShowFog = false, game crash when I go to the next map "Test2",
    
    - Old Map saving (knowing that this map was visited and is now cleared)

---
+++ src/game/interactables/DialogTrigger.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../Collidable.h"
+#include <string>
+
+class DialogTrigger : public Collidable
+{
+private:
+    std::string text_;
+    std::string name_;
+    std::string portrait_path_;
+    bool is_in_range_ = false;
+
+public:
+    DialogTrigger(Rectangle hitbox, std::string text, std::string name, std::string portrait);
+    ~DialogTrigger() override = default;
+
+    void Tick(float delta_time) override;
+    void Draw() override;
+    void On_Collision(std::shared_ptr<Collidable> other) override;
+    Collision_Type Get_Collision_Type() const override;
+
+    const std::string& GetText() const;
+    const std::string& GetName() const;
+    const std::string& GetPortraitPath() const;
+};
---
+++ src/scenes/DialogManager.cpp
@@ -0,0 +1,70 @@
+#include "DialogManager.h"
+#include "AssetManager.h"
+#include "../config.h.in"
+
+DialogManager::DialogManager()
+{
+    dialog_font_ = LoadFontEx("assets/fonts/alagard.ttf", 250, nullptr, 0);
+    SetTextureFilter(dialog_font_.texture, TEXTURE_FILTER_POINT);
+}
+
+DialogManager::~DialogManager()
+{
+    UnloadFont(dialog_font_);
+}
+
+void DialogManager::ShowDialog(const std::string& text, const std::string& name, const std::string& portrait_path)
+{
+    current_text_ = text;
+    character_name_ = name;
+
+    if (!portrait_path.empty()) {
+        portrait_texture_ = AssetManager::GetInstance().Load(portrait_path.c_str());
+        has_portrait_ = true;
+    } else {
+        has_portrait_ = false;
+    }
+
+    is_active_ = true;
+}
+
+void DialogManager::HideDialog()
+{
+    is_active_ = false;
+}
+
+bool DialogManager::IsActive() const
+{
+    return is_active_;
+}
+
+void DialogManager::Draw()
+{
+    if (!is_active_) return;
+
+    // --- Box-Positionierung zwischen den UI-Elementen ---
+    float box_height = 220;
+    float box_y = game::Config::kStageHeight - box_height - 20;
+    Rectangle box_rect = { 350, box_y, (float)game::Config::kStageWidth - 700, box_height };
+
+    // --- Box zeichnen ---
+    DrawRectangleRec(box_rect, Fade(BLACK, 0.8f));
+    DrawRectangleLinesEx(box_rect, 3, Fade(WHITE, 0.6f));
+
+    // --- Porträt (rechts) ---
+    float text_start_x = box_rect.x + 30;
+    float text_end_x = box_rect.x + box_rect.width - 30;
+    if (has_portrait_) {
+        float portrait_size = box_height - 40;
+        DrawTextureEx(portrait_texture_, {box_rect.x + box_rect.width - portrait_size - 20, box_rect.y + 20}, 0.0f, portrait_size / portrait_texture_.height, WHITE);
+        text_end_x -= (portrait_size + 40);
+    }
+
+    // --- Name (falls vorhanden) ---
+    if (!character_name_.empty()) {
+        DrawTextEx(dialog_font_, character_name_.c_str(), {text_start_x, box_rect.y + 20}, 50, 2, Color{ 216, 176, 168, 255 });
+        DrawTextEx(dialog_font_, current_text_.c_str(), {text_start_x, box_rect.y + 90}, 45, 2, Fade(WHITE, 0.9f));
+    } else {
+        DrawTextEx(dialog_font_, current_text_.c_str(), {text_start_x, box_rect.y + 40}, 45, 2, Fade(WHITE, 0.9f));
+    }
+}
