50da296da25a6358f96e8fa7dd4f76b0705219c2
Andreas <manzandreas29@hotmail.com>
2025-09-10 23:32:35 +0200

    Working State 0.9.4 - for Game release
    Current State update:
    - Big Update
    
    Working:
    - Item Spawning
    - movWalls
    - Bombs
    - Heal + Dmg for Player
    - Dmg for Enemies
    - Spawner
    - Fog
    - useFog mechanic
    - Enemies spawning
    - Animation Loading for Player
    - Dying State for Player
    - Projectiles for Player
    - Melee/Sweep for BOTH
    - UI
    - keyWall
    - HP Drain
    - breakWalls
    - Doors (Room Changing)
    - Score system
    - Animation Loading Enemy
    - Debug Placeholders for Enemies
    - Range Enemy
    - special enemy spawner
    - Spawn lags are away
    - Maps
    - reticle
    - Sound
    
    Coming Next:
    - Dialog system
    
    Not Working rn:
    - if kDebugShowFog = false, game crash when I go to the next map "Test2",
    
    - Old Map saving (knowing that this map was visited and is now cleared)

--- src/scenes/LevelScreen.cpp
+++ src/scenes/LevelScreen.cpp
@@ -242,6 +242,38 @@ void LevelScreen::LoadGameObjects(Object_Manager& g_objectManager) {
                     Rectangle rect = { (float)object.getPosition().x, (float)object.getPosition().y, (float)object.getSize().x, (float)object.getSize().y };
                     new_object = std::make_shared<Door>(rect, target_map, target_spawn);
                 }
+                else if (object_name == "doors2")
+                {
+                    // Lese die Custom Properties aus Tiled aus.
+                    std::string target_map = "default.json";
+                    std::string target_spawn = "player_start";
+
+                    if (object.getProperties().hasProperty("target_map")) {
+                        target_map = object.getProperties().getValue<std::string>("target_map");
+                    }
+                    if (object.getProperties().hasProperty("target_spawn_point")) {
+                        target_spawn = object.getProperties().getValue<std::string>("target_spawn_point");
+                    }
+                    // Erstelle die Hitbox und das Door-Objekt.
+                    Rectangle rect = { (float)object.getPosition().x, (float)object.getPosition().y, (float)object.getSize().x, (float)object.getSize().y };
+                    new_object = std::make_shared<Door>(rect, target_map, target_spawn);
+                }
+                else if (object_name == "doors3")
+                {
+                    // Lese die Custom Properties aus Tiled aus.
+                    std::string target_map = "default.json";
+                    std::string target_spawn = "player_start";
+
+                    if (object.getProperties().hasProperty("target_map")) {
+                        target_map = object.getProperties().getValue<std::string>("target_map");
+                    }
+                    if (object.getProperties().hasProperty("target_spawn_point")) {
+                        target_spawn = object.getProperties().getValue<std::string>("target_spawn_point");
+                    }
+                    // Erstelle die Hitbox und das Door-Objekt.
+                    Rectangle rect = { (float)object.getPosition().x, (float)object.getPosition().y, (float)object.getSize().x, (float)object.getSize().y };
+                    new_object = std::make_shared<Door>(rect, target_map, target_spawn);
+                }
                 else if (object_name == "statue")
                 {
                     // 1. Lese alle notwendigen Properties aus Tiled
