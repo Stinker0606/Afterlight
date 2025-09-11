6eab6bdda0664da3f5dbfa11d2af9fe9df0bb605
LPMad <mad.lp120@gmail.com>
2025-09-11 23:20:21 +0200

    TILESET UPDATE BITTE PULLEN

--- assets/Tiled/Tilesets/ALLE_TILESETS.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="ALLE_TILESETS" tilewidth="32" tileheight="32" tilecount="435" columns="15">
- <image source="ALLE_TILESETS.png" width="480" height="928"/>
-</tileset>
